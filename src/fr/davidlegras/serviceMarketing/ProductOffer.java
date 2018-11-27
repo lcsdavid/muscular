c8a071a4469867b63bc8856a5a7fafef2a4f2d40
ThLegras <legrastheo@gmail.com>
2018-11-27 08:43:40 +0100

    complétion de product offer
    attention il faut ajouter une exception au niveau du constructeur de product offer

--- src/fr/davidlegras/serviceMarketing/ProductOffer.java
+++ src/fr/davidlegras/serviceMarketing/ProductOffer.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 public class ProductOffer extends CommercialOffer {
     public ProductOffer (float reduction, String target){
+        if(reduction > 100)
+            reduction = 0;//créer une exception
         this.reduction = reduction;
         this.target = target;
     }
@@ -19,16 +21,18 @@ public class ProductOffer extends CommercialOffer {
         return target;
     }
 
-    public float getPrice(Product product){
+    public float getPrice(Product product){//on renvoie la valeur du produit après réduction
         if(product.getCategorie().equals(target) || product.getName().equals(target))
             return (product.getPrice() - (product.getPrice()*reduction/100));
         return product.getPrice();//le prix du produit ne change pas avec cette réduction
     }
 
-    public float getPrice(ArrayList<Product> products){
+    public float getReduction(ArrayList<Product> products){
         float res = 0;
         for(Product p : products){
-            res += getPrice(p);
+            if(p.getCategorie().equals(target) || p.getName().equals(target))
+                res += (p.getPrice() - (p.getPrice()*reduction/100));
+            else { res += p.getPrice();}
         }
         return res;
     }
